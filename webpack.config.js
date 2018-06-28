230aed225d88c6cd13e82acce78d8972ac77c1b2
kanayannet <kanayannet@gmail.com>
2018-06-28 17:09:40 +0900

    babel was added for es6 -> es5 converted

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,19 @@ module.exports = {
     filename: 'index.js',
     path: path.join(__dirname, './')
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.common.js'
     }
   }
-};
+}
